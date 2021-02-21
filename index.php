9578bd4dbbbbf7f62c859b1d28c9df89b6aad45e
futebolmultimilionario <79179613+futebolmultimilionario@users.noreply.github.com>
2021-02-21 00:20:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,25 +6,25 @@ $texto = $updateArray["message"]["text"];
 
 $arrayTexto = preg_split("/\r\n|\n|\r/", $texto);
 
-$arrayTexto[2] = substr($arrayTexto[2], 8);
-$arrayTexto[3] = preg_split("/\s+/", $arrayTexto[3]);
+$arrayTexto[0] = substr($arrayTexto[0], 8);
+$arrayTexto[2] = preg_split("/\s+/", $arrayTexto[2]);
 
-$time1 = preg_split("/\b @ \b/iu", $arrayTexto[5]);
-$time2 = preg_split("/\b @ \b/iu", $arrayTexto[7]);
+$time1 = preg_split("/\b @ \b/iu", $arrayTexto[4]);
+$time2 = preg_split("/\b @ \b/iu", $arrayTexto[6]);
 
-$empate = preg_split("/\b @ \b/iu", $arrayTexto[6]);
+$empate = preg_split("/\b @ \b/iu", $arrayTexto[5]);
 
-$arrayTexto[3] = strtotime(DateTime::createFromFormat("d/m/Y H:i", $arrayTexto[3][1]."/2021 ".$arrayTexto[3][2])->format('m/d/Y H:i:s')); // define data desejada
+$arrayTexto[2] = strtotime(DateTime::createFromFormat("d/m/Y H:i", $arrayTexto[2][1]."/2021 ".$arrayTexto[2][2])->format('m/d/Y H:i:s')); // define data desejada
 
 $db_handle = pg_connect("host=ec2-54-164-241-193.compute-1.amazonaws.com dbname=detfg6vttnaua8 port=5432 user=kgsgrroozfzpnv password=a2ec0dd00478fd02c6395df74d3e82adc94632e51ea2c1cca2ba94f988e591f5");
 
 if($arrayTexto[0] == "Abertura"){
-	$query = "INSERT INTO tabelateste (campeonato, data, time1, oddtime1, oddempate, time2, oddtime2, link) VALUES ('$arrayTexto[2]', '$arrayTexto[3]', '$time1[0]', '$time1[1]', '$empate[1]', '$time2[0]', '$time2[1]', '$arrayTexto[9]')";
+	$query = "INSERT INTO tabelateste (campeonato, data, time1, oddtime1, oddempate, time2, oddtime2, link) VALUES ('$arrayTexto[0]', '$arrayTexto[2]', '$time1[0]', '$time1[1]', '$empate[1]', '$time2[0]', '$time2[1]', '$arrayTexto[8]')";
 	$result = pg_query($db_handle, $query);
 
 }
 if($arrayTexto[0] == "Reabertura"){
-	$query = "UPDATE tabelateste SET campeonato='$arrayTexto[2]', data='$arrayTexto[3]', oddtime1='$time1[1]', oddempate='$empate[1]', time2='$time2[0]', oddtime2='$time2[1]', link='$arrayTexto[9]' WHERE time1='$time1[0]' and time2='$time2[0]'";
+	$query = "UPDATE tabelateste SET campeonato='$arrayTexto[0]', data='$arrayTexto[2]', oddtime1='$time1[1]', oddempate='$empate[1]', time2='$time2[0]', oddtime2='$time2[1]', link='$arrayTexto[8]' WHERE time1='$time1[0]' and time2='$time2[0]'";
 	$result = pg_query($db_handle, $query);
 
  
